fcc22ffbee9a6325439fdb331da8d3dde8ca92e6
Joshua Glepko <jglepko@gmail.com>
2024-10-14 12:06:59 -0400

    added initial files to repo

---
+++ c/src/main.c
@@ -0,0 +1,20 @@
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+#include "vm.h"
+
+int main(int argc, const char* argv[]) {
+  initVM();
+
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
+
+  freeVM();
+  return 0;
+}
